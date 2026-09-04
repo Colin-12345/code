06abaaad48e7110b9819f0ebb931a7780a4d6bba
colin.lundberg <colin.lundberg@elev.ntig.se>
2026-09-04 08:29:41 +0200

    skol uppgift

---
+++ prg1/uppgift1.py
@@ -0,0 +1,6 @@
+name = input('enter your name: ')
+age = int(input('enter your age: '))
+length = float(input('enter your length in meters: '))
+favint = int(input('enter your favorite number: '))
+
+print(f'Jag heter {name} och jag är {age} år gammal. Jag är {length} meter lång och mitt favoritnummer är {favint}.')
